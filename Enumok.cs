a0eaf7a1c9c45bcaf768eba4fc44095b84d2709c
OG5001 <olahgergo917@gmail.com>
2026-02-20 09:16:34 +0100

    Kezelő osztály kész + Classok

---
+++ Enumok.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uveghazprojekt
+{
+	enum Szerepkor{
+		KERTESZ,
+		TECHNIKUS,
+		ADMIN
+	}
+
+	internal class Enumok
+	{
+	}
+}
